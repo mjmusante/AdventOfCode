cdc3518f3ba40cd75697488fdce15b8d6c86c9b4
Mark J Musante <mark.musante@gmail.com>
2016-12-02 06:46:32 -0500

    changes to prep for part 2

--- 2016/day02.py
+++ 2016/day02.py
@@ -8,42 +8,42 @@ TRANS = {
         "U": 0, "D": 1, "L": 2, "R": 3,
     }
 
-TRACK = {
-        1: (1, 4, 1, 2),
-        2: (2, 5, 1, 3),
-        3: (3, 6, 2, 3),
-        4: (1, 7, 4, 5),
-        5: (2, 8, 4, 6),
-        6: (3, 9, 5, 6),
-        7: (4, 7, 7, 8),
-        8: (5, 8, 7, 9),
-        9: (6, 9, 8, 9),
+TRACK_pt1 = {
+        "1": ("1", "4", "1", "2"),
+        "2": ("2", "5", "1", "3"),
+        "3": ("3", "6", "2", "3"),
+        "4": ("1", "7", "4", "5"),
+        "5": ("2", "8", "4", "6"),
+        "6": ("3", "9", "5", "6"),
+        "7": ("4", "7", "7", "8"),
+        "8": ("5", "8", "7", "9"),
+        "9": ("6", "9", "8", "9"),
     }
 
 
-def get_code(start, seq):
+def get_code(start, seq, track):
     for s in seq:
-        start = TRACK[start][TRANS[s]]
+        start = track[start][TRANS[s]]
     return start
 
-def find_code(steps):
-    pos = 5
+def find_code(steps, track):
+    pos = "5"
     rslt = ""
     for s in steps:
-        pos = get_code(pos, s)
+        pos = get_code(pos, s, track)
         rslt = "%s%s" % (rslt, pos)
 
-    return int(rslt)
+    return rslt
 
-tests = {
-        ( "ULL", "RRDDD", "LURDL", "UUUUD", ): 1985
+test_pt1 = {
+        ( "ULL", "RRDDD", "LURDL", "UUUUD", ): "1985"
 }
 
-for t in tests:
-    ans = find_code(t)
-    if ans != tests[t]:
-        print("Expecting %s, got %s" % (tests[t], ans))
+for t in test_pt1:
+    ans = find_code(t, TRACK_pt1)
+    if ans != test_pt1[t]:
+        print("Expecting %s, got %s" % (test_pt1[t], ans))
         sys.exit(1)
 
 code = [line.strip() for line in open("day02.txt")]
-print("Code: %s" % find_code(code))
+print("Part 1 code: %s" % find_code(code, TRACK_pt1))
