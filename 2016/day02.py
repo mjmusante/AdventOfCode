d4e4fddbcb7d2d872fc4e7b0adaaff722c09ec69
Mark J Musante <mark.musante@gmail.com>
2016-12-02 06:51:56 -0500

    finished day 2

--- 2016/day02.py
+++ 2016/day02.py
@@ -20,6 +20,21 @@ TRACK_pt1 = {
         "9": ("6", "9", "8", "9"),
     }
 
+TRACK_pt2 = {
+        "1": ("1", "3", "1", "1"),
+        "2": ("2", "6", "2", "3"),
+        "3": ("1", "7", "2", "4"),
+        "4": ("4", "8", "3", "4"),
+        "5": ("5", "5", "5", "6"),
+        "6": ("2", "A", "5", "7"),
+        "7": ("3", "B", "6", "8"),
+        "8": ("4", "C", "7", "9"),
+        "9": ("9", "9", "8", "9"),
+        "A": ("6", "A", "A", "B"),
+        "B": ("7", "D", "A", "C"),
+        "C": ("8", "C", "B", "C"),
+        "D": ("B", "D", "D", "D"),
+    }
 
 def get_code(start, seq, track):
     for s in seq:
@@ -39,11 +54,22 @@ test_pt1 = {
         ( "ULL", "RRDDD", "LURDL", "UUUUD", ): "1985"
 }
 
+test_pt2 = {
+        ( "ULL", "RRDDD", "LURDL", "UUUUD", ): "5DB3"
+}
+
 for t in test_pt1:
     ans = find_code(t, TRACK_pt1)
     if ans != test_pt1[t]:
         print("Expecting %s, got %s" % (test_pt1[t], ans))
         sys.exit(1)
 
+for t in test_pt2:
+    ans = find_code(t, TRACK_pt2)
+    if ans != test_pt2[t]:
+        print("Expecting %s, got %s" % (test_pt2[t], ans))
+        sys.exit(1)
+
 code = [line.strip() for line in open("day02.txt")]
 print("Part 1 code: %s" % find_code(code, TRACK_pt1))
+print("Part 2 code: %s" % find_code(code, TRACK_pt2))
