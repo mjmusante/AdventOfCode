7e8417619fd2017be87fd9f061273f6fc630ef07
Mark J Musante <mark.musante@gmail.com>
2016-12-06 06:38:43 -0500

    code cleanup

--- 2016/day06.py
+++ 2016/day06.py
@@ -7,15 +7,12 @@ from collections import Counter
 import sys
 
 
-def get_code(cyphertext, first):
+def get_code(cyphertext, pos=0):
     result = ""
     codelen = len(cyphertext[0])    # assumption: all strings are same length
     for i in range(0, codelen):
         calc = Counter([c[i] for c in cyphertext])
-        if first:
-            let, cnt = calc.most_common(1)[0]
-        else:
-            let, cnt = calc.most_common()[-1]
+        let, cnt = calc.most_common()[pos]
         result += let
     return result
 
@@ -41,14 +38,14 @@ testdata = [
 ]
 
 
-if get_code(testdata, first=True) != "easter":
+if get_code(testdata) != "easter":
     print("oops - easter busticated")
     sys.exit(1)
-if get_code(testdata, first=False) != "advent":
+if get_code(testdata, pos=-1) != "advent":
     print("oops - advent busticated")
     sys.exit(1)
 
 cyphertext = [line.strip() for line in open("day06.txt")]
 
-print(" Most frequent: %s" % get_code(cyphertext, first=True))
-print("Least frequent: %s" % get_code(cyphertext, first=False))
+print(" Most frequent: %s" % get_code(cyphertext))
+print("Least frequent: %s" % get_code(cyphertext, pos=-1))
