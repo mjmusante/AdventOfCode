315807c5c44e2eab7ad1de771ccef77f65805395
Mark J Musante <mark.musante@gmail.com>
2016-12-06 06:31:58 -0500

    second gold star for day 6

--- 2016/day06.py
+++ 2016/day06.py
@@ -7,11 +7,15 @@ from collections import Counter
 import sys
 
 
-def get_code(cyphertext):
+def get_code(cyphertext, first):
     result = ""
     codelen = len(cyphertext[0])    # assumption: all strings are same length
     for i in range(0, codelen):
-        let, cnt = Counter([c[i] for c in cyphertext]).most_common(1)[0]
+        calc = Counter([c[i] for c in cyphertext])
+        if first:
+            let, cnt = calc.most_common(1)[0]
+        else:
+            let, cnt = calc.most_common()[-1]
         result += let
     return result
 
@@ -37,8 +41,14 @@ testdata = [
 ]
 
 
-if get_code(testdata) != "easter":
-    print("oops - busticated")
+if get_code(testdata, first=True) != "easter":
+    print("oops - easter busticated")
+    sys.exit(1)
+if get_code(testdata, first=False) != "advent":
+    print("oops - advent busticated")
     sys.exit(1)
 
-print("message: %s" % get_code([line.strip() for line in open("day06.txt")]))
+cyphertext = [line.strip() for line in open("day06.txt")]
+
+print(" Most frequent: %s" % get_code(cyphertext, first=True))
+print("Least frequent: %s" % get_code(cyphertext, first=False))
