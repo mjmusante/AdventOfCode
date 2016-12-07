f7cfb24c5ee48567195d20bdeb8e96005b0f4c6f
Mark J Musante <mark.musante@gmail.com>
2016-12-07 08:17:31 -0500

    second part of day 7

--- 2016/day07.py
+++ 2016/day07.py
@@ -29,25 +29,71 @@ def supports_tls(seq):
         else:
             return possible or contains_pair(seq)
 
-testdata = {
+def supports_ssl(seq):
+    slist = list()
+    hlist = list()
+
+    while True:
+        m = PAIR.search(seq)
+        if m:
+            (snet, hnet) = m.group(1, 2)
+            slist.append(snet)
+            hlist.append(hnet)
+            seq = seq[len(m.group(0)):]
+        else:
+            slist.append(seq)
+            break
+    
+    for s in slist:
+        for i in range(len(s) - 2):
+            if s[i] != s[i + 1] and s[i] == s[i + 2]:
+                sub = s[i + 1] + s[i] + s[i + 1]
+                for h in hlist:
+                    if sub in h:
+                        return True
+
+    return False
+
+
+test_tls = {
         "abba[mnop]qrst": True,
         "abcd[bddb]xyyx": False,
         "aaaa[qwer]tyui": False,
         "ioxxoj[asdfgh]zxcvbn": True,
     }
 
-for addr in testdata:
+for addr in test_tls:
     rslt = supports_tls(addr)
-    if rslt != testdata[addr]:
+    if rslt != test_tls[addr]:
         print("Error: %s returned %s but expecting %s" %
-                (addr, rslt, testdata[addr]))
+                (addr, rslt, test_tls[addr]))
+        sys.exit(1)
+
+test_ssl = {
+        "aba[bab]xyz": True,
+        "xyx[xyx]xyx": False,
+        "aaa[kek]eke": True,
+        "zazbz[bzb]cdb": True,
+    }
+
+for addr in test_ssl:
+    rslt = supports_ssl(addr)
+    if rslt != test_ssl[addr]:
+        print("Error: %s returned %s but expecting %s" %
+                (addr, rslt, test_ssl[addr]))
         sys.exit(1)
 
 
 addrs = [line.strip() for line in open("day07.txt")]
+
 count = 0
 for a in addrs:
     if supports_tls(a):
         count += 1
-
 print("%s IPs support TLS" % count)
+
+count = 0
+for a in addrs:
+    if supports_ssl(a):
+        count += 1
+print("%s IPs support SSL" % count)
