a6db7b08a5106b1882103c8b933fd85578f59960
Mark J Musante <mark.musante@gmail.com>
2016-12-09 10:09:09 -0500

    day 9 second gold star: recursive decompression length calculation

--- 2016/day09.py
+++ 2016/day09.py
@@ -8,7 +8,7 @@ import sys
 CODE = re.compile(r"\((\d+)x(\d+)\)")
 
 
-def decompress(string):
+def decompress_v1(string):
     rslt = ""
     i = 0
     while i < len(string):
@@ -28,8 +28,29 @@ def decompress(string):
 
     return rslt
 
+def decompress_len(string):
+    rslt = 0
+    i = 0
+    while i < len(string):
+        if string[i] == "(":
+            m = CODE.match(string[i:])
+            if not m:
+                print("Error parsing %s..." % string[i:i+10])
+                sys.exit(1)
+            (l, r) = m.group(1, 2)
+            i += len(m.group(0))
+            substr = string[i:i+int(l)]
+            rslt += decompress_len(substr) * int(r)
+            i += int(l)
+        else:
+            rslt += 1
+            i += 1
 
-testdata = {
+    return rslt
+
+
+
+testdata1 = {
         "ADVENT": "ADVENT",
         "A(1x5)BC": "ABBBBBC",
         "(3x3)XYZ": "XYZXYZXYZ",
@@ -38,13 +59,27 @@ testdata = {
         "X(8x2)(3x3)ABCY": "X(3x3)ABC(3x3)ABCY",
     }
 
-for t in testdata:
-    l = decompress(t)
-    if l != testdata[t]:
-        print("For '%s': expecting %s, got %s" % (t, testdata[t], l))
+testdata2 = {
+        "(3x3)XYZ": 9,
+        "X(8x2)(3x3)ABCY": 20,
+        "(27x12)(20x12)(13x14)(7x10)(1x12)A": 241920,
+        "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN": 445,
+    }
+
+for t in testdata1:
+    l = decompress_v1(t)
+    if l != testdata1[t]:
+        print("For '%s': expecting %s, got %s" % (t, testdata1[t], l))
+        sys.exit(1)
+
+for t in testdata2:
+    l = decompress_len(t)
+    if l != testdata2[t]:
+        print("For '%s': expecting %s, got %s" % (t, testdata2[t], l))
         sys.exit(1)
 
 with open("day09.txt") as f:
     data = f.read().strip()
 
-print(len(decompress(data)))
+print("V1 Decompress length: %s" % len(decompress_v1(data)))
+print("V2 Decompress length: %s" % decompress_len(data))
