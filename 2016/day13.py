ac0ee9b16b5d5d85287dbe5b4c818d1d651d94aa
mjmusante <mark.musante@gmail.com>
2017-11-20 16:52:34 -0500

    cleaned up phase 1 - added phase 2

--- 2016/day13.py
+++ 2016/day13.py
@@ -5,9 +5,12 @@ class FirstFloor:
 
     def __init__(self, floor):
         self.designer = floor
+        self.entries = dict()
+        self.visited = dict()
+        self.lowest = 100
 
 
-    def get(self, x, y):
+    def get_old_way(self, x, y):
         base = x * x + 3 * x + 2 * x * y + y + y * y
         base += self.designer
         numones = bin(base).count("1")
@@ -15,22 +18,54 @@ class FirstFloor:
             return self.SPACE
         return self.WALL
 
+    def get(self, x, y):
+        if (x, y) not in self.entries:
+            base = x * x + 3 * x + 2 * x * y + y + y * y
+            base += self.designer
+            numones = bin(base).count("1")
+            if numones % 2 == 0:
+                self.entries[(x, y)] = self.SPACE
+            else:
+                self.entries[(x, y)] = self.WALL
+        return self.entries[(x, y)]
+
     def dump(self, max_x, max_y):
         for y in range(max_y):
             str = "%3d " % y
             for x in range(max_x):
-                if self.get(x, y) == self.SPACE:
-                    str += "."
+                if (x, y) in self.visited:
+                    if self.get(x, y) != self.SPACE:
+                        print("ERROR on (%s, %s)" % (x, y))
+                    else:
+                        str += "o"
                 else:
-                    str += "#"
+                    if self.get(x, y) == self.SPACE:
+                        str += "."
+                    else:
+                        str += "#"
             print(str)
 
     def adjacent_spaces(self, midx, midy):
         for pos in [(midx - 1, midy), (midx, midy - 1),
                     (midx + 1, midy), (midx, midy + 1)]:
+            if pos[0] < 0 or pos[1] < 0:
+                continue
             if self.get(*pos) == self.SPACE:
                 yield pos
 
+
+    def visit_max_locs(self, start, path=[], lim=50):
+        path = path + [start]
+        if len(path) > lim + 1:
+            return None
+        if self.get(*start) == self.WALL:
+            return None
+        self.visited[start] = True
+        # print(path)
+        for node in self.adjacent_spaces(*start):
+            if node not in path:
+                self.visit_max_locs(node, path, lim)
+        return len(self.visited)
 
     def find_shortest_path(self, start, end, path=[]):
         path = path + [start]
@@ -38,7 +73,7 @@ class FirstFloor:
             return path
         if start[0] > end[0] + 10 or start[1] > end[1] + 10:
             return None
-        if len(path) > 150:
+        if len(path) > self.lowest:
             return None
         if self.get(*start) == self.WALL:
             return None
@@ -47,12 +82,18 @@ class FirstFloor:
             if node not in path:
                 newpath = self.find_shortest_path(node, end, path)
                 if newpath:
-                    if not shortest or len(newpath) < len(shortest):
+                    if shortest:
+                        l = len(shortest)
+                    else:
+                        l = len(newpath)
+                    if not shortest or len(newpath) < l:
                         shortest = newpath
-                        print(len(shortest))
+                        if l < self.lowest:
+                            self.lowest = l
         return shortest
 
 if __name__ == "__main__":
     ff = FirstFloor(1350)
-    print("steps to (31, 39) not including first = %s" %
-          len(ff.find_shortest_path((1, 1), (31, 39))) - 1)
+    steps = len(ff.find_shortest_path((1, 1), (31, 39)))
+    print("steps to (31, 39) not including first = %s" % (steps - 1))
+    print("max places at 50 limit: %s" % (ff.visit_max_locs((1, 1))))
