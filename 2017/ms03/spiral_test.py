0bce2a9292ba00916e389e6e3d803e5325d6bbf9
Mark Musante <mark.musante@gmail.com>
2017-12-06 17:27:14 -0500

    forgot to push unit tests

--- 2017/ms03/spiral_test.py
+++ 2017/ms03/spiral_test.py
@@ -1,26 +1,22 @@
 import unittest
 
-from spiral import Spiral
+from spiral import coords_of, sum_larger_than
 
 class TestSpiral(unittest.TestCase):
 
     def test_steps_from_1(self):
-        s = Spiral()
-        self.assertEquals(s.steps_from_1(1), 0)
-        self.assertEquals(s.steps_from_1(12), 3)
-        self.assertEquals(s.steps_from_1(23), 2)
-        self.assertEquals(s.steps_from_1(1024), 31)
+        x, y = coords_of(1)
+        self.assertEquals(int(abs(x) + abs(y)), 0)
 
-    def test_coords(self):
-        s = Spiral()
-        self.assertEquals(s.coords_of(1), (0, 0))
-        self.assertEquals(s.coords_of(22), (-1, -2))
-        self.assertEquals(s.coords_of(13), (2, 2))
+        x, y = coords_of(12)
+        self.assertEquals(int(abs(x) + abs(y)), 3)
+
+        x, y = coords_of(23)
+        self.assertEquals(int(abs(x) + abs(y)), 2)
+
+        x, y = coords_of(1024)
+        self.assertEquals(int(abs(x) + abs(y)), 31)
+
 
     def test_larger(self):
-        s = Spiral()
-        self.assertEquals(s.sum_larger_than(7), 10)
-
-    # def test_summer(self):
-    #     s = Spiral()
-    #     self.assertEquals(s.sum_larger_than(1), 2)
+        self.assertEquals(sum_larger_than(7), 10)
