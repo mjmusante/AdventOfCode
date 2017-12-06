7ba98b3133afeb74cb6e0153bef3c80af058107e
Mark Musante <mark.musante@gmail.com>
2017-12-06 14:24:31 -0500

    refactor

--- 2017/ms05/jumper.py
+++ 2017/ms05/jumper.py
@@ -2,9 +2,8 @@ def count_jumps(memory):
     ip = 0
     jumps = 0
     while ip >= 0 and ip < len(memory):
-        last_ip = ip
-        ip += memory[ip]
-        memory[last_ip] += 1
+        memory[ip] += 1
+        ip += memory[ip] - 1
         jumps += 1
     return jumps
 
@@ -12,26 +11,20 @@ def count_wacky_jumps(memory):
     ip = 0
     jumps = 0
     while ip >= 0 and ip < len(memory):
-        if len(memory) < 10: print(memory)
-        last_ip = ip
-        last_val = memory[ip]
-        ip += last_val
-        if last_val >= 3:
-            memory[last_ip] = last_val - 1
-            if len(memory) < 10:
-                print('%s -> %s' % (last_ip, memory[last_ip]))
+        if memory[ip] >= 3:
+            memory[ip] -= 1
+            ip += memory[ip] + 1
         else:
-            memory[last_ip] = last_val + 1
-            if len(memory) < 10:
-                print('%s -> %s' % (last_ip, memory[last_ip]))
+            memory[ip] += 1
+            ip += memory[ip] - 1
         jumps += 1
     return jumps
 
 if __name__ == "__main__":
-    print("Test: %s" % count_jumps([0, 3, 0, 1, -3]))
+    assert(count_jumps([0, 3, 0, 1, -3]) == 5)
     puzzle = [int(line.strip()) for line in open("puzzle_data.txt")]
     print("Part 1: %s" % count_jumps(puzzle))
 
-    print("Test Wacky: %s" % count_wacky_jumps([0, 3, 0, 1, -3]))
+    assert(count_wacky_jumps([0, 3, 0, 1, -3]) == 10)
     puzzle = [int(line.strip()) for line in open("puzzle_data.txt")]
     print("Part 2: %s" % count_wacky_jumps(puzzle))
