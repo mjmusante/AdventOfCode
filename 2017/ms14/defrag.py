ae7114327361d27e3009eb7264aa68b29e09099c
Mark Musante <mark.musante@gmail.com>
2017-12-14 08:41:04 -0500

    2017 millisecond 14

---
+++ 2017/ms14/defrag.py
@@ -0,0 +1,48 @@
+import sys
+sys.path.append("..")
+
+from ms10.knot import calc_hash
+
+def hex_to_bin(hexstr):
+    ans = ""
+    for c in hexstr:
+        b = bin(int(c, 16))[2:]
+        ans += "0000"[len(b):] + b
+    assert(len(ans) == 128)
+    return ans
+
+
+groups = 0
+
+def is_group(grid, x, y):
+    if grid[x][y] != '1':
+        return 0
+
+    def visit_group(x, y):
+        if x < 0 or x > 127 or y < 0 or y > 127:
+            return
+        if grid[x][y] != '1':
+            return
+        grid[x][y] = '2'
+        visit_group(x - 1, y)
+        visit_group(x, y - 1)
+        visit_group(x + 1, y)
+        visit_group(x, y + 1)
+
+    visit_group(x, y)
+    return 1
+
+if __name__ == "__main__":
+    count = 0
+    grid = []
+    for i in range(128):
+        h = calc_hash("jzgqcdpd-%s" % i)
+        b = hex_to_bin(h)
+        grid.append(list(b))
+        count  += b.count('1')
+    print("Part 1: %s" % count)
+
+    for x in range(128):
+        for y in range(128):
+            groups += is_group(grid, x, y)
+    print("Part 2: %s" % groups)
