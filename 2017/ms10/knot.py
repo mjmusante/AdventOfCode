29eda88cb6e3475954a0e05ec604a7baaf385bb4
Mark Musante <mark.musante@gmail.com>
2017-12-14 08:40:49 -0500

    module-ify knot calculation code

--- 2017/ms10/knot.py
+++ 2017/ms10/knot.py
@@ -38,28 +38,29 @@ def calc_hash(ary):
     return answer
 
 
-test_data = "3,4,1,5"
-puzzle_data = "183,0,31,146,254,240,223,150,2,206,161,1,255,232,199,88"
+if __name__ == "__main__":
+    test_data = "3,4,1,5"
+    puzzle_data = "183,0,31,146,254,240,223,150,2,206,161,1,255,232,199,88"
 
-(tpos, tskip, tdata) = crypt([int(t) for t in test_data.split(",")], size=5)
-print("test data: %s" % (tdata[tpos] * tdata[(tpos + 1) % 5]))
+    (tpos, tskip, tdata) = crypt([int(t) for t in test_data.split(",")], size=5)
+    print("test data: %s" % (tdata[tpos] * tdata[(tpos + 1) % 5]))
 
-(ppos, pskip, pdata) = crypt([int(p) for p in puzzle_data.split(",")])
-print("Part 1: %s" % (pdata[ppos] * pdata[(ppos + 1) % 256]))
+    (ppos, pskip, pdata) = crypt([int(p) for p in puzzle_data.split(",")])
+    print("Part 1: %s" % (pdata[ppos] * pdata[(ppos + 1) % 256]))
 
-dense = {
-    "": "a2582a3a0e66e6e86e3812dcb672a272",
-    "AoC 2017": "33efeb34ea91902bb2f59c9920caa6cd",
-    "1,2,3": "3efbe78a8d82f29979031a4aa0b16a9d",
-    "1,2,4": "63960835bcdc130f0b66d7ff4f6a5a8e"
-}
+    dense = {
+        "": "a2582a3a0e66e6e86e3812dcb672a272",
+        "AoC 2017": "33efeb34ea91902bb2f59c9920caa6cd",
+        "1,2,3": "3efbe78a8d82f29979031a4aa0b16a9d",
+        "1,2,4": "63960835bcdc130f0b66d7ff4f6a5a8e"
+    }
 
-for d in dense:
-    answer = calc_hash(d)
-    if answer != dense[d]:
-        print("Input string '%s'\n\tgot '%s'\n\texpecting '%s'" % (d, answer, dense[d]))
-        break
-    else:
-        print("string '%s' validated" % d)
+    for d in dense:
+        answer = calc_hash(d)
+        if answer != dense[d]:
+            print("Input string '%s'\n\tgot '%s'\n\texpecting '%s'" % (d, answer, dense[d]))
+            break
+        else:
+            print("string '%s' validated" % d)
 
-print("Part 2: %s" % calc_hash(puzzle_data))
+    print("Part 2: %s" % calc_hash(puzzle_data))
