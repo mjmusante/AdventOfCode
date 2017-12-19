e729625f4c2c75e29696c90716b85dc208707055
Mark Musante <mark.musante@gmail.com>
2017-12-18 19:22:14 -0500

    refactor

--- 2017/ms04/passphrase_test.py
+++ 2017/ms04/passphrase_test.py
@@ -2,7 +2,9 @@ import unittest
 
 from passphrase import count_valid
 
+
 class TestPassphrase(unittest.TestCase):
+
     def test_validity(self):
         lines = ["aa bb cc dd ee", "aa bb cc dd aa", "aa bb cc dd aaa"]
         self.assertEquals(count_valid(lines), 2)
@@ -15,3 +17,11 @@ class TestPassphrase(unittest.TestCase):
             "iiii oiii ooii oooi oooo",
             "oiii ioii iioi iiio"]
         self.assertEquals(count_valid(lines, True), 3)
+
+    def test_can_pass_part_1(self):
+        puzzle = [line.strip() for line in open("puzzle_data.txt")]
+        self.assertEquals(count_valid(puzzle), 325)
+
+    def test_can_pass_part_2(self):
+        puzzle = [line.strip() for line in open("puzzle_data.txt")]
+        self.assertEquals(count_valid(puzzle, True), 119)
