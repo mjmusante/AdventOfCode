b5766e91eda33a443409fa0c9d4107f2ac639778
Mark Musante <mark.musante@gmail.com>
2019-12-02 10:40:46 -0500

    day 2 part 2

--- 2019/day02/src/main.rs
+++ 2019/day02/src/main.rs
@@ -4,6 +4,31 @@ use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 
+fn try_values(program: &Vec<i64>, noun: i64, verb: i64) -> i64 {
+    let mut mem = program.clone();
+    let mut pos = 0;
+
+    mem[1] = noun;
+    mem[2] = verb;
+
+    loop {
+        let (val, loc) = match mem[pos] {
+            1 => { pos += 4; (mem[mem[pos - 3] as usize] + mem[mem[pos - 2] as usize], mem[pos - 1]) }
+            2 => { pos += 4; (mem[mem[pos - 3] as usize] * mem[mem[pos - 2] as usize], mem[pos - 1]) }
+            99 => { (-1, -1) }
+            _ => { return -1; }
+        };
+
+        if loc < 0 {
+            break;
+        }
+        mem[loc as usize] = val
+    }
+
+    mem[0]
+}
+
+
 fn main() {
     let f = File::open("data/input.txt").unwrap();
     let vlist =  BufReader::new(&f)
@@ -15,25 +40,16 @@ fn main() {
         .split(",")
         .map(|num| num.parse::<i64>().unwrap())
         .collect::<Vec<i64>>();
-    
-    let mut part1 = ary.clone();
-    let mut pos = 0;
 
-    part1[1] = 12;
-    part1[2] = 2;
-
-    loop {
-        let (val, loc) = match part1[pos] {
-            1 => { pos += 4; (part1[part1[pos - 3] as usize] + part1[part1[pos - 2] as usize], part1[pos - 1]) }
-            2 => { pos += 4; (part1[part1[pos - 3] as usize] * part1[part1[pos - 2] as usize], part1[pos - 1]) }
-            99 => { (-1, -1) }
-            _ => { println!("failure; got {} as a value", part1[pos]); exit(1); }
-        };
-
-        if loc < 0 {
-            break;
+    println!("part 1 = {}", try_values(&ary, 12, 2));
+    for i in 0..99 {
+        for j in 0..99 {
+            let v = try_values(&ary, i, j);
+            if v == 19690720 {
+                println!("part 2 = noun: {}, verb: {}, code = {}", i, j, 100 * i + j);
+                exit(0);
+            }
         }
-        part1[loc as usize] = val
     }
-    println!("val at 0 is {}", part1[0]);
+    println!("part 2 = FAILED");
 }
