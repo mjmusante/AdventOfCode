5123afaa40534413567c067455970b261b829a99
Mark Musante <mark.musante@gmail.com>
2019-12-05 07:18:22 -0500

    initial refactor

--- 2019/day05/src/main.rs
+++ 2019/day05/src/main.rs
@@ -1,8 +1,8 @@
-use std::process::exit;
-
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
+
+static OPCOUNT: [i64; 9] = [0, 2, 2, 0, 1, 2, 2, 2, 2];
 
 fn intcode(program: &Vec<i64>, id: i64) -> i64 {
     let mut mem = program.clone();
@@ -12,46 +12,46 @@ fn intcode(program: &Vec<i64>, id: i64) -> i64 {
         let opcode = mem[pos] % 100;
         let src1 = (mem[pos] / 100) % 10;
         let src2 = (mem[pos] / 1000) % 10;
+        let (op1, op2);
         pos += 1;
 
-        //println!("opcode = {}", opcode);
+        if OPCOUNT[opcode as usize] > 0  {
+            op1 = if src1 == 0 { mem[mem[pos] as usize] } else { mem[pos] };
+            pos += 1;
+        } else {
+            op1 = 0;
+        }
+        if OPCOUNT[opcode as usize] > 1 {
+            op2 = if src2 == 0 { mem[mem[pos] as usize] } else { mem[pos] };
+            pos += 1;
+        } else {
+            op2 = 0;
+        }
 
         let (val, loc) = match opcode {
             1 => {
-                let op1 = if src1 == 0 { mem[mem[pos] as usize] } else { mem[pos] };
-                let op2 = if src2 == 0 { mem[mem[pos + 1] as usize] } else { mem[pos + 1] };
-                pos += 3;
+                pos += 1;
                 (op1 + op2, mem[pos - 1])
              }
             2 => {
-                let op1 = if src1 == 0 { mem[mem[pos] as usize] } else { mem[pos] };
-                let op2 = if src2 == 0 { mem[mem[pos + 1] as usize] } else { mem[pos + 1] };
-                pos += 3;
+                pos += 1;
                 (op1 * op2, mem[pos - 1])
              }
             3 => { pos += 1; (id, mem[pos - 1]) }
             4 => {
-                let op1 = if src1 == 0 { mem[mem[pos] as usize] } else { mem[pos] };
-                pos += 1;
                 if op1 != 0 {
                     return op1;
                 }
                 (0, -1)
              }
             5|6 => {
-                let op1 = if src1 == 0 { mem[mem[pos] as usize] } else { mem[pos] };
-                let op2 = if src2 == 0 { mem[mem[pos + 1] as usize] } else { mem[pos + 1] };
                 if (opcode == 5 && op1 != 0) || (opcode == 6 && op1 == 0) {
                     pos = op2 as usize;
-                } else {
-                    pos += 2;
                 }
                 (0, -1)
              }
             7 => {
-                let op1 = if src1 == 0 { mem[mem[pos] as usize] } else { mem[pos] };
-                let op2 = if src2 == 0 { mem[mem[pos + 1] as usize] } else { mem[pos + 1] };
-                pos += 3;
+                pos += 1;
                 if op1 < op2 {
                     (1, mem[pos - 1])
                 } else {
@@ -59,9 +59,7 @@ fn intcode(program: &Vec<i64>, id: i64) -> i64 {
                 }
              }
             8 => {
-                let op1 = if src1 == 0 { mem[mem[pos] as usize] } else { mem[pos] };
-                let op2 = if src2 == 0 { mem[mem[pos + 1] as usize] } else { mem[pos + 1] };
-                pos += 3;
+                pos += 1;
                 if op1 == op2 {
                     (1, mem[pos - 1])
                 } else {
@@ -95,14 +93,6 @@ fn main() {
         .split(",")
         .map(|num| num.parse::<i64>().unwrap())
         .collect::<Vec<i64>>();
-
-    /*
-    let foo = "1002,4,3,4,33".split(",")
-        .map(|num| num.parse::<i64>().unwrap())
-        .collect::<Vec<i64>>();
-
-    println!("test: {}", intcode(&foo));
-    */
 
     println!("part 1 = {}", intcode(&ary, 1));
     println!("part 1 = {}", intcode(&ary, 5));
