955eb76b6ceed47be9f58daeec53bdca210dd04e
Mark Musante <mark.musante@gmail.com>
2019-12-17 07:53:24 -0500

    2019 day 17 part 2

--- 2019/day17/src/main.rs
+++ 2019/day17/src/main.rs
@@ -60,10 +60,72 @@ fn main() {
                         part1 += x * y;
                     }
                 }
-                print!("{}", ch);
+                // print!("{}", ch);
             }
         }
-        println!("");
+        // println!("");
     }
     println!("part 1 = {}", part1);
+
+    // reset and scan forward to a double newline
+    c.reset();
+    c.poke(0, 2);
+    c.run();
+    let mut last_c = 0;
+    while c.has_output() {
+        let ch = c.next_output();
+        if last_c == 10 && ch == 10 {
+            break;
+        }
+        last_c = ch;
+    }
+
+    /*
+    Hardcoded:
+
+    A    L,10,L,12,R,6,
+    B    R,10,L,4,L,4,L,12,
+    A    L,10,L,12,R,6,
+    B    R,10,L,4,L,4,L,12,
+    A    L,10,L,12,R,6,
+    C    L,10,R,10,R,6,L,4,
+    B    R,10,L,4,L,4,L,12,
+    C    L,10,R,10,R,6,L,4,
+    A    L,10,L,12,R,6,
+    C    L,10,R,10,R,6,L,4
+
+    A: L,10,L,12,R,6
+    B: R,10,L,4,L,4,L,12
+    C: L,10,R,10,R,6,L,4
+    */
+
+    let main = "A,B,A,B,A,C,B,C,A,C";
+    let proga = "L,10,L,12,R,6";
+    let progb = "R,10,L,4,L,4,L,12";
+    let progc = "L,10,R,10,R,6,L,4";
+    let video = "n";
+
+    for instr in [main, proga, progb, progc, video].iter() {
+        while c.has_output() {
+            // print!("{}", c.next_output() as u8 as char);
+            c.next_output();
+        }
+        for val in instr.chars().map(|c| c as i64) {
+            c.run_with_input(val);
+        }
+        c.run_with_input(10);
+    }
+
+    // scan forward again to the next double-newline
+    last_c = 0;
+    while c.has_output() {
+        let ch = c.next_output();
+        if last_c == 10 && ch == 10 {
+            break;
+        }
+        last_c = ch;
+    }
+
+    assert!(c.has_output());
+    println!("part 2 = {}", c.next_output());
 }
