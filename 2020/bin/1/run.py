2c8882aebf60ccff0ad93ff91ae0b51911f39752
Mark Musante <mark.musante@gmail.com>
2020-10-23 21:27:34 -0400

    messing around with directory structure

---
+++ 2020/bin/1/run.py
@@ -0,0 +1,5 @@
+from lib.lines import lines
+
+
+vals = lines("data/day01.txt")
+print("line count is %s" % len(vals))
---
+++ 2020/bin/lib/lines.py
@@ -0,0 +1,2 @@
+def lines(pathname):
+    return [line.strip() for line in open(pathname)]
