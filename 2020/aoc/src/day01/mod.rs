f40784023afb4c17a4d9e019d86f670aa21e179b
Mark J Musante <mark.musante@oracle.com>
2020-12-01 09:26:21 -0500

    use filter instead of if

--- 2020/aoc/src/day01/mod.rs
+++ 2020/aoc/src/day01/mod.rs
@@ -4,17 +4,22 @@ use aoc::utils::nums;
 
 pub fn day01() {
     let v = nums("data/01.txt");
-    for pair in v.clone().into_iter().combinations(2) {
-        if pair[0] + pair[1] == 2020 {
-            println!("Part 1 = {}", pair[0] * pair[1]);
-            break;
-        }
+    for pair in v
+        .clone()
+        .into_iter()
+        .combinations(2)
+        .filter(|x| x[0] + x[1] == 2020)
+    {
+        println!("Part 1 = {}", pair[0] * pair[1]);
+        break;
     }
 
-    for pair in v.clone().into_iter().combinations(3) {
-        if pair[0] + pair[1] + pair[2] == 2020 {
-            println!("Part 2 = {}", pair[0] * pair[1] * pair[2]);
-            break;
-        }
+    for trip in v
+        .into_iter()
+        .combinations(3)
+        .filter(|x| x[0] + x[1] + x[2] == 2020)
+    {
+        println!("Part 2 = {}", trip[0] * trip[1] * trip[2]);
+        break;
     }
 }
