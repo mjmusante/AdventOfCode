78f328b4d8699bdd657f7e0f3bc3c6235bcae518
Mark J Musante <mark.musante@oracle.com>
2020-12-02 09:09:40 -0500

    timing each of the days

--- 2020/aoc/src/main.rs
+++ 2020/aoc/src/main.rs
@@ -1,7 +1,16 @@
+use std::time::Instant;
+
 mod day01;
 mod day02;
 
 fn main() {
+    let now = Instant::now();
     day01::day01();
+    let elapsed = now.elapsed().as_millis();
+    println!(" -> Time: {}ms", elapsed);
+
+    let now = Instant::now();
     day02::day02();
+    let elapsed = now.elapsed().as_millis();
+    println!(" -> Time: {}ms", elapsed);
 }
