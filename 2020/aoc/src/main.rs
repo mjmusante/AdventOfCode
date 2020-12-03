879e9be065f55a049f5dd648a860b0b3c9ea105b
Mark J Musante <mark.musante@oracle.com>
2020-12-03 09:20:09 -0500

    added overall time for execution

--- 2020/aoc/src/main.rs
+++ 2020/aoc/src/main.rs
@@ -1,3 +1,4 @@
+use core::time::Duration;
 use std::time::Instant;
 
 mod day01;
@@ -8,11 +9,15 @@ type Func = fn();
 
 fn main() {
     let runners: Vec<Func> = vec![day01::run, day02::run, day03::run];
+    let mut total = Duration::new(0, 0);
 
     for run in runners {
         let now = Instant::now();
         run();
-        let elapsed = now.elapsed().as_millis();
-        println!(" -> Time: {}ms", elapsed);
+        let elapsed = now.elapsed();
+
+        total += elapsed;
+        println!(" -> Time: {}ms", elapsed.as_millis());
     }
+    println!("\nTotal time: {}ms", total.as_millis());
 }
