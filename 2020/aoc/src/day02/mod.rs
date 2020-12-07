a7f12a6224830298b1b880e71b80e1233cc4c7dc
Mark J Musante <mark.musante@oracle.com>
2020-12-07 13:05:38 -0500

    refactor to add tests

--- 2020/aoc/src/day02/mod.rs
+++ 2020/aoc/src/day02/mod.rs
@@ -3,22 +3,12 @@ use aoc::utils::lines;
 use regex::Regex;
 
 pub fn run() {
-    let reg = Regex::new(r"(\d+)-(\d+) (.): (.*)").unwrap();
-    if check(&reg, "1-3 a: abcde") != (true, true) {
-        panic!("failure in case 1");
-    }
-    if check(&reg, "1-3 b: cdefg") != (false, false) {
-        panic!("failure in case 2");
-    }
-    if check(&reg, "2-9 c: ccccccccc") != (true, false) {
-        panic!("failure in case 3");
-    }
-
+    let c = Checker::new();
     let mut p1valid = 0;
     let mut p2valid = 0;
     let data = lines("data/02.txt");
     for d in data {
-        let (part1, part2) = check(&reg, &d);
+        let (part1, part2) = c.check(&d);
         if part1 {
             p1valid += 1;
         }
@@ -30,25 +20,58 @@ pub fn run() {
     println!("Part 2 = {}", p2valid);
 }
 
-fn check(reg: &Regex, line: &str) -> (bool, bool) {
-    let foo = reg.captures_iter(line).next().unwrap();
-    let low = foo[1].parse::<i64>().unwrap();
-    let high = foo[2].parse::<i64>().unwrap();
-    let letter = foo[3].to_string().chars().next().unwrap();
-    let passwd = foo[4].to_string();
+struct Checker {
+    reg: Regex,
+}
 
-    let mut count = 0;
-    let mut pvalid = false;
-    let mut loc = 1;
-    for n in passwd.chars() {
-        if n == letter {
-            count += 1;
-            if loc == low || loc == high {
-                pvalid = !pvalid;
-            }
-        }
-        loc += 1;
+impl Checker {
+    pub fn new() -> Checker {
+        Checker { reg: Regex::new(r"(\d+)-(\d+) (.): (.*)").unwrap() }
     }
 
-    (count >= low && count <= high, pvalid)
+    pub fn check(&self, line: &str) -> (bool, bool) {
+        let foo = self.reg.captures_iter(line).next().unwrap();
+        let low = foo[1].parse::<i64>().unwrap();
+        let high = foo[2].parse::<i64>().unwrap();
+        let letter = foo[3].to_string().chars().next().unwrap();
+        let passwd = foo[4].to_string();
+
+        let mut count = 0;
+        let mut pvalid = false;
+        let mut loc = 1;
+        for n in passwd.chars() {
+            if n == letter {
+                count += 1;
+                if loc == low || loc == high {
+                    pvalid = !pvalid;
+                }
+            }
+            loc += 1;
+        }
+
+        (count >= low && count <= high, pvalid)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let c = Checker::new();
+        assert_eq!(c.check("1-3 a: abcde"), (true, true));
+    }
+
+    #[test]
+    fn test2() {
+        let c = Checker::new();
+        assert_eq!(c.check("1-3 b: cdefg"), (false, false));
+    }
+
+    #[test]
+    fn test3() {
+        let c = Checker::new();
+        assert_eq!(c.check("2-9 c: ccccccccc"), (true, false));
+    }
 }
