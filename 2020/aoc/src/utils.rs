2ec7ba839ce1f3b49a575aee657e13205073670a
Mark Musante <mark.musante@gmail.com>
2020-12-13 11:18:50 -0500

    code cleanup

--- 2020/aoc/src/utils.rs
+++ 2020/aoc/src/utils.rs
@@ -38,25 +38,19 @@ pub fn mod_inv(num: i128, modulus: i128) -> Option<i128> {
     let mdl = modulus.abs();
     let val = if num < 0 { mdl - (-num % mdl) } else { num };
 
-    let mut t = 0;
-    let mut nt = 1;
-    let mut r = mdl;
-    let mut nr = val % mdl;
-    while nr != 0 {
-        let q = r / nr;
-        let tmp = nt;
-        nt = t - q * nt;
-        t = tmp;
-        let tmp = nr;
-        nr = r - q * nr;
-        r = tmp;
+    let mut t = (0, 1);
+    let mut r = (mdl, val % mdl);
+    while r.1 != 0 {
+        let q = r.0 / r.1;
+        t = (t.1, t.0 - q * t.1);
+        r = (r.1, r.0 - q * r.1);
     }
 
-    if r > 1 {
+    if r.0 > 1 {
         return None;
     }
-    if t < 0 {
-        t += modulus;
+    if t.0 < 0 {
+        t.0 += modulus;
     }
-    Some(t)
+    Some(t.0)
 }
