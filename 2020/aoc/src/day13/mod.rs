39eb9315dd203408c9056f9349b29ebbbca2ba79
Mark Musante <mark.musante@gmail.com>
2020-12-13 08:03:07 -0500

    add in the x's as 0

--- 2020/aoc/src/day13/mod.rs
+++ 2020/aoc/src/day13/mod.rs
@@ -13,18 +13,33 @@ struct Schedule {
 }
 
 fn parse(lines: &Vec<String>) -> Schedule {
-    let mut sched = Schedule { timestamp: 0, bus: Vec::new() };
+    let mut sched = Schedule {
+        timestamp: 0,
+        bus: Vec::new(),
+    };
 
     sched.timestamp = lines.get(0).unwrap().parse().expect("invalid timeval");
 
-    sched.bus = lines.get(1).unwrap().split(",").filter(|x| *x != "x")
-        .map(|val| val.parse().expect("invalid bus id")).collect();
+    sched.bus = lines
+        .get(1)
+        .unwrap()
+        .split(",")
+        .map(|val| match val {
+            "x" => 0,
+            val => val.parse().expect("invalid bus id"),
+        })
+        .collect();
 
     sched
 }
 
 fn next_bus(sched: &Schedule) -> i64 {
-    let wait_times : Vec<(i64, i64)> = sched.bus.iter().map(|x| (*x, x - sched.timestamp % x)).collect();
+    let wait_times: Vec<(i64, i64)> = sched
+        .bus
+        .iter()
+        .filter(|x| **x != 0)
+        .map(|x| (*x, x - sched.timestamp % x))
+        .collect();
 
     let mut min = wait_times[0];
     for w in 1..wait_times.len() {
@@ -41,8 +56,7 @@ mod test {
     use super::*;
 
     fn test_data() -> Vec<String> {
-        vec!["939".to_string(),
-             "7,13,x,x,59,x,31,19".to_string()]
+        vec!["939".to_string(), "7,13,x,x,59,x,31,19".to_string()]
         // vec!["1007153".to_string(),
         //      "29,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,37,x,x,x,x,x,433,x,x,x,x,x,x,x,x,x,x,x,x,13,17,x,x,x,x,19,x,x,x,23,x,x,x,x,x,x,x,977,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,41".to_string()]
     }
