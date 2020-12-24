ca6f9de5fafaa8bd330d72e0e832d2e17f492bf2
Mark Musante <mark.musante@gmail.com>
2020-12-24 08:24:50 -0500

    2020 day 24 part 2

--- 2020/aoc/src/day24/mod.rs
+++ 2020/aoc/src/day24/mod.rs
@@ -4,24 +4,18 @@ use aoc::utils::lines;
 
 pub fn run() {
     let data = lines("data/24.txt");
+    let ans = flip_and_count(&data);
 
-    println!("Part 1 = {}", flip_and_count(&data));
+    println!("Part 1 = {}", ans.0);
+    println!("Part 2 = {}", ans.1);
 }
 
-// east: x + 1, y
-// west: x - 1, y
-// southeast: x + 1 if y is odd, y + 1
-// southwest: x - 1 if y is even, y + 1
-// northeast: x + 1 if y is odd, y - 1,
-// northwest: x - 1 if y is even, y - 1
-// enum HexDir {
-//     East, SouthEast, SouthWest, West, NorthWest, NorthEast
-// }
-
-// use HexDir::*;
-
-fn flip_and_count(list: &Vec<String>) -> i64 {
+fn flip_and_count(list: &Vec<String>) -> (i64, i64) {
     let mut floor = HashSet::new();
+    let mut min_x = 0;
+    let mut min_y = 0;
+    let mut max_x = 0;
+    let mut max_y = 0;
 
     for i in list {
         let mut x = 0;
@@ -87,17 +81,69 @@ fn flip_and_count(list: &Vec<String>) -> i64 {
         } else {
             floor.insert((x, y));
         }
+        if max_x < x {
+            max_x = x;
+        }
+        if min_x > x {
+            min_x = x;
+        }
+        if max_y < y {
+            max_y = y;
+        }
+        if min_y > y {
+            min_y = y;
+        }
     }
-    floor.len() as i64
+
+    let part1 = floor.len() as i64;
+
+    // east, southeast, southwest, west, northwest, northeast
+    let odd_y = vec![(1, 0), (1, 1), (0, 1), (-1, 0), (0, -1), (1, -1)];
+    let even_y = vec![(1, 0), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1)];
+
+    // now play life
+    for i in 0..100 {
+        let mut new_floor = HashSet::new();
+        min_x -= 1; // we'll search an area that's
+        min_y -= 1; // bigger than we need to, but
+        max_x += 1; // it's good enough for AoC
+        max_y += 1; // Burma Shave
+
+        for x in min_x..=max_x {
+            for y in min_y..=max_y {
+                let search = if y & 1 == 0 { &even_y } else { &odd_y };
+                let mut count = 0;
+                for s in search {
+                    if floor.contains(&(x + s.0, y + s.1)) {
+                        count += 1;
+                    }
+                }
+                if floor.contains(&(x, y)) {
+                    if count == 1 || count == 2 {
+                        // stays black
+                        new_floor.insert((x, y));
+                    }
+                } else {
+                    if count == 2 {
+                        // turns black
+                        new_floor.insert((x, y));
+                    }
+                }
+            }
+        }
+
+        floor = new_floor;
+    }
+
+    (part1, floor.len() as i64)
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
 
-    #[test]
-    fn test1() {
-        let v = vec![
+    fn test_data() -> Vec<String> {
+        vec![
             "sesenwnenenewseeswwswswwnenewsewsw".to_string(),
             "neeenesenwnwwswnenewnwwsewnenwseswesw".to_string(),
             "seswneswswsenwwnwse".to_string(),
@@ -118,21 +164,28 @@ mod test {
             "eneswnwswnwsenenwnwnwwseeswneewsenese".to_string(),
             "neswnwewnwnwseenwseesewsenwsweewe".to_string(),
             "wseweeenwnesenwwwswnew".to_string(),
-        ];
+        ]
+    }
 
-        assert_eq!(flip_and_count(&v), 10);
+    #[test]
+    fn test1() {
+        let v = test_data();
+        let ans = flip_and_count(&v);
+
+        assert_eq!(ans.0, 10);
+        assert_eq!(ans.1, 2208);
     }
 
     #[test]
     fn test2() {
         let v = vec![
-            "senwswne".to_string(),           // should be (0, 0) (turn black)
-            "eeee".to_string(),               // should be (4, 0) (turn black)
-            "sesesese".to_string(),           // should be (2, 4) (turn black)
-            "eeeeswswswsw".to_string(),       // should be (2, 4) (turn white)
-            "eeesesesese".to_string(),        // should be (5, 4) (turn black)
-            "sesesesenenenene".to_string(),   // should be (4, 0) (turn white)
+            "senwswne".to_string(),         // should be (0, 0) (turn black)
+            "eeee".to_string(),             // should be (4, 0) (turn black)
+            "sesesese".to_string(),         // should be (2, 4) (turn black)
+            "eeeeswswswsw".to_string(),     // should be (2, 4) (turn white)
+            "eeesesesese".to_string(),      // should be (5, 4) (turn black)
+            "sesesesenenenene".to_string(), // should be (4, 0) (turn white)
         ];
-        assert_eq!(flip_and_count(&v), 2);
+        assert_eq!(flip_and_count(&v).0, 2);
     }
 }
