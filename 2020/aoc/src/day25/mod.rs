f310496f13106bbac1df7966fe27f99d60cb3183
Mark Musante <mark.musante@gmail.com>
2020-12-25 06:48:23 -0500

    2020 day 25

--- 2020/aoc/src/day25/mod.rs
+++ 2020/aoc/src/day25/mod.rs
@@ -1,7 +1,34 @@
-pub fn run() {}
+use aoc::utils::nums;
 
-fn solve() -> i64 {
-    0
+pub fn run() {
+    let vals = nums("data/25.txt");
+
+    println!("Part 1 = {}", solve(vals[0], vals[1]));
+
+}
+
+
+fn find_loop_of(key: i64) -> i64 {
+    let mut ans = 7;
+    let mut count = 1;
+
+    while ans != key {
+        ans = (ans * 7) % 20201227;
+        count += 1;
+    }
+
+    count
+}
+
+fn solve(door_pubkey: i64, card_pubkey: i64) -> i64 {
+    let card_loop = find_loop_of(card_pubkey);
+    let mut ans = door_pubkey;
+
+    for _ in 1..card_loop {
+        ans = (ans * door_pubkey) % 20201227;
+    }
+
+    ans
 }
 
 #[cfg(test)]
@@ -9,7 +36,14 @@ mod test {
     use super::*;
 
     #[test]
-    fn test() {
-        assert_eq!(solve(), 0);
+    fn test1() {
+        assert_eq!(find_loop_of(17807724), 11);
+        assert_eq!(find_loop_of(5764801), 8);
+
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(solve(17807724, 5764801), 14897079);
     }
 }
