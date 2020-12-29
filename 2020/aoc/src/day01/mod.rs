2e45e3728b7b196f2bbd0b8563d8bde1ce571fc3
Mark Musante <mark.musante@gmail.com>
2020-12-29 18:02:18 -0500

    use option

--- 2020/aoc/src/day01/mod.rs
+++ 2020/aoc/src/day01/mod.rs
@@ -7,22 +7,22 @@ use aoc::utils::nums;
 pub fn run() {
     let v = nums("data/01.txt");
 
-    println!("Part 1 = {}", prod_sum(&v, 2));
-    println!("Part 2 = {}", prod_sum(&v, 3));
+    println!("Part 1 = {}", prod_sum(&v, 2).unwrap());
+    println!("Part 2 = {}", prod_sum(&v, 3).unwrap());
 }
 
-fn prod_sum(v: &Vec<i64>, n: usize) -> i64 {
+fn prod_sum(v: &Vec<i64>, n: usize) -> Option<i64> {
     let hs: HashSet<i64> = HashSet::from_iter(v.iter().cloned());
     let iv = hs.iter().cloned().combinations(n - 1);
 
     for inverse in iv {
         let tot: i64 = inverse.iter().sum();
         if hs.contains(&(2020 - tot)) {
-            return inverse.iter().fold(2020 - tot, |prod, &mul| prod * mul);
+            return Some(inverse.iter().fold(2020 - tot, |prod, &mul| prod * mul));
         }
     }
 
-    0
+    None
 }
 
 #[cfg(test)]
@@ -32,12 +32,12 @@ mod test {
     #[test]
     fn test1() {
         let v: Vec<i64> = vec![1721, 979, 366, 299, 675, 1456];
-        assert_eq!(prod_sum(&v, 2), 514579);
+        assert_eq!(prod_sum(&v, 2).unwrap(), 514579);
     }
 
     #[test]
     fn test2() {
         let v: Vec<i64> = vec![1721, 979, 366, 299, 675, 1456];
-        assert_eq!(prod_sum(&v, 3), 241861950);
+        assert_eq!(prod_sum(&v, 3).unwrap(), 241861950);
     }
 }
