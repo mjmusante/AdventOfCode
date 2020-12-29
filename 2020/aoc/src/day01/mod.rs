d7f30fcf62090c1911626efb3adf130a98d1f4ed
Mark Musante <mark.musante@gmail.com>
2020-12-29 13:51:48 -0500

    2020 day 1 - rewritten to be generic over n

--- 2020/aoc/src/day01/mod.rs
+++ 2020/aoc/src/day01/mod.rs
@@ -1,32 +1,28 @@
 use itertools::Itertools;
+use std::collections::HashSet;
+use std::iter::FromIterator;
 
 use aoc::utils::nums;
 
 pub fn run() {
     let v = nums("data/01.txt");
 
-    println!("Part 1 = {}", part1(&v));
-    println!("Part 2 = {}", part2(&v));
+    println!("Part 1 = {}", prod_sum(&v, 2));
+    println!("Part 2 = {}", prod_sum(&v, 3));
 }
 
-fn part1(v: &Vec<i64>) -> i64 {
-    v.iter()
-        .combinations(2)
-        .filter(|x| x[0] + x[1] == 2020)
-        .nth(0)
-        .unwrap()
-        .iter()
-        .fold(1, |prod, &mul| prod * mul)
-}
+fn prod_sum(v: &Vec<i64>, n: usize) -> i64 {
+    let hs : HashSet::<i64> = HashSet::from_iter(v.iter().cloned());
+    let iv = hs.iter().cloned().combinations(n - 1);
 
-fn part2(v: &Vec<i64>) -> i64 {
-    v.iter()
-        .combinations(3)
-        .filter(|x| x[0] + x[1] + x[2] == 2020)
-        .nth(0)
-        .unwrap()
-        .iter()
-        .fold(1, |prod, &mul| prod * mul)
+    for inverse in iv {
+        let tot : i64 = inverse.iter().sum();
+        if hs.contains(&(2020 - tot)) {
+            return inverse.iter().fold(2020 - tot, |prod, &mul| prod *  mul);
+        }
+    }
+
+    0
 }
 
 #[cfg(test)]
@@ -36,12 +32,12 @@ mod test {
     #[test]
     fn test1() {
         let v: Vec<i64> = vec![1721, 979, 366, 299, 675, 1456];
-        assert_eq!(part1(&v), 514579);
+        assert_eq!(prod_sum(&v, 2), 514579);
     }
 
     #[test]
     fn test2() {
         let v: Vec<i64> = vec![1721, 979, 366, 299, 675, 1456];
-        assert_eq!(part2(&v), 241861950);
+        assert_eq!(prod_sum(&v, 3), 241861950);
     }
 }
